0a5563cf716ffe26263fe754daa98c0255096bd3
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-10-24 14:58:01 +0200

    update

--- signup.php
+++ signup.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Signup</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="space"></div>
 
+				<form class="sign-form" action="get_signup.php" method="POST">
+					<div class="form-group">
+						<input class="form-control" type="text" name="username" placeholder="username">
+					</div>
+					<div class="form-group">
+						<input class="form-control" type="email" name="mail" placeholder="email-address">
+					</div>
+					<div class="form-group">
+						<input class="form-control" type="password" name="pass" placeholder="password">
+					</div>
+					<div class="form-group">
+						<input class="form-control" type="password" name="confirm_pass" placeholder="confirm password">
+					</div>
+					<button type="submit" class="btn btn-primary">SIGNUP</button>
+				</form>
+
+</body>
+</html>
