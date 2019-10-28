354da05b12cd66d112158732ae6f713d4600f958
x.r.methula@gmail.com <ray>
2019-10-28 19:31:36 +0200

    update

--- resetpassword.php
+++ resetpassword.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Signup</title>
+	<title>Reset Password</title>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" href="assets/css/style.css">
@@ -13,14 +13,10 @@
 	<div class="space"></div>
 
 	<div class="signup-wrapper">
-		<h2 class="signup-heading">SignUp</h2>
-		<form action="get_signup.php" autocomplete="off" method="POST">
-			<input class="signup-input" type="text" name="username" placeholder="username">
+		<h2 class="signup-heading">Reset</h2>
+		<form action="get_resetpassword.php" autocomplete="off" method="POST">
 			<input class="signup-input" type="email" name="email" placeholder="email-address">
-			<input class="signup-input" type="password" name="password" placeholder="password">
-			<input class="signup-input" type="password" name="confirm" placeholder="confirm password">
-			<a class="signup-login" href="signin.php"><p>already have an account?</p></a>
-			<button class="btn-signup" type="submit" name="signup-submit">SIGNUP</button>
+			<button class="btn-signup" type="submit" name="signup-submit">RESET</button>
 		</form>
 	</div>
 </body>
--- signin.php
+++ signin.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Signup</title>
+	<title>Signin</title>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" href="assets/css/style.css">
@@ -13,14 +13,13 @@
 	<div class="space"></div>
 
 	<div class="signup-wrapper">
-		<h2 class="signup-heading">SignUp</h2>
-		<form action="get_signup.php" autocomplete="off" method="POST">
+		<h2 class="signup-heading">SignIn</h2>
+		<form action="get_signin.php" autocomplete="off" method="POST">
 			<input class="signup-input" type="text" name="username" placeholder="username">
-			<input class="signup-input" type="email" name="email" placeholder="email-address">
 			<input class="signup-input" type="password" name="password" placeholder="password">
-			<input class="signup-input" type="password" name="confirm" placeholder="confirm password">
-			<a class="signup-login" href="signin.php"><p>already have an account?</p></a>
-			<button class="btn-signup" type="submit" name="signup-submit">SIGNUP</button>
+			<a class="signup-login" href="signup.php"><p>create an account</p></a>
+			<button class="btn-signup" type="submit" name="signup-submit">SIGNIN</button>
+			<a class="reset-pass" href="resetpassword.php"><p>reset password</p></a>
 		</form>
 	</div>
 </body>
