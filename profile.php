a596c81b9c79af3055f833cd2bc71cb032146209
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-11-05 15:51:49 +0200

    update

--- profile.php
+++ profile.php
@@ -1,8 +1,40 @@
 <?php
 	session_start();
-
-	if ($_SESSION['userId'])
-		echo "SignedIn" . "<br>";
-	else
-		echo "SignedOut" . "<br>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Profile</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="space"></div>
+
+
+	<div class="profile-wrapper">
+		<div class="profile-img"></div>
+		<h3 class="username">xmethula</h3>
+		<p class="email">xmethula@student.wethinkcode.co.za</p>
+		<div class="line-1"></div>
+		<div class="statistics">
+			<div class="posts">
+				<p class="num">17</p>
+				<p class="text">POSTS</p>
+			</div>
+			<div class="likes">
+				<p class="num">105</p>
+				<p class="text">LIKES</p>
+			</div>
+		</div>
+		<div class="line-2"></div>
+		<p class="active-date">Active since: 09 SEP 2019</p>
+		<a href=""><div class="btn-personal">EDIT PERSONAL INFO</div></a>
+	</div>
+</body>
+</html>
