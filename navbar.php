c1acd62ba2db62331b88f55856cf3f4f6afbf2ce
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-11-05 18:00:21 +0200

    update

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Navbar</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="nav-container">
+		<div class="nav-wrapper">
+			<h2 class="logo">CAMAGRU</h2>
+			<nav>
+				<ul class="nav-links">
+					<li><a href="3">Gallery</a></li>
+					<li><a href="3">Post</a></li>
+					<li><a href="3">Profile</a></li>
+				</ul>
+			</nav>
+			<a href=""><div class="cta">Signout</div></a>
+		</div>
+	</div>
+</body>
+</html>
